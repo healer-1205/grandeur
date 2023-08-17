957189618d87436630cfa5f52d09e3b88aee0f65
Healer <91068182+healer-1205@users.noreply.github.com>
2023-08-17 16:08:53 +0800

    make custom cursor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import { Home } from "./pages/Home"
-import Cursor from "react-cursor-follow"
 
 function App() {
   return (
     <>
-      <Cursor duration={0.8} size={35} color="#eb5939" />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,10 +3,13 @@ import { Header } from "../../components/Header"
 import { Social } from "../../components/Social"
 import { Grandeur } from "../../components/Grandeur"
 import { AboutMe } from "../../components/AboutMe"
+// import Cursor from "react-cursor-follow"
+import { Cursor } from "../../components/Cursor"
 
 export const Home = () => {
   return (
     <div>
+      <Cursor />
       <Header />
       <Social />
       <div className="relative sm:mt-36 mt-28">
